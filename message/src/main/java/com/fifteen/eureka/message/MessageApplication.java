1fb7d01a04c50d110fde57e3708f976ae888af30
신영한 <syhan7516@naver.com>
2024-12-10 20:46:16 +0900

    feat: message project 생성 #5

---
+++ message/src/main/java/com/fifteen/eureka/message/MessageApplication.java
@@ -0,0 +1,15 @@
+package com.fifteen.eureka.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class MessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageApplication.class, args);
+	}
+
+}
