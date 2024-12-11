65e1348cadd7c38331aa0bbbb6ba3983052302ce
geon8692 <geon4735@gmail.com>
2024-12-12 01:38:45 +0900

    feat: Hub 기능 구현(복구) #6

---
+++ delivery/src/main/java/com/fifteen/eureka/delivery/common/auditor/AuditorProvider.java
@@ -0,0 +1,18 @@
+package com.fifteen.eureka.delivery.common.auditor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+
+@Component
+public class AuditorProvider {
+
+  @Getter
+  private static AuditorAwareImpl auditorAware;
+
+  @Autowired
+  public AuditorProvider(AuditorAwareImpl auditorAware) {
+    AuditorProvider.auditorAware = auditorAware;
+  }
+}
---
+++ delivery/src/main/java/com/fifteen/eureka/delivery/domain/model/ManagerType.java
@@ -0,0 +1,5 @@
+package com.fifteen.eureka.delivery.domain.model;
+
+public enum ManagerType {
+	HUB, VENDOR
+}
