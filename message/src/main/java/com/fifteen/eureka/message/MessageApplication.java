548df3b478755923bcfa519a03f5bbd21103fa0d
신영한 <syhan7516@naver.com>
2024-12-12 20:36:48 +0900

    feat: common submodule 등록 및 설정 #5

--- message/src/main/java/com/fifteen/eureka/message/MessageApplication.java
+++ message/src/main/java/com/fifteen/eureka/message/MessageApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"com.fifteen.eureka.message",
+		"com.fifteen.eureka.common"})
 @EnableFeignClients
 public class MessageApplication {
 
