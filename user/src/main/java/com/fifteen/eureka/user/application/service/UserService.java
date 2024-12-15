3e66aac76d16b2b187e53986578b32595c672b20
sojinchoi <dvega.lyla@gmail.com>
2024-12-15 16:09:14 +0900

    feat: 회원 가입 승인 기능 구현 #10

--- user/src/main/java/com/fifteen/eureka/user/application/service/UserService.java
+++ user/src/main/java/com/fifteen/eureka/user/application/service/UserService.java
@@ -1,8 +1,12 @@
 package com.fifteen.eureka.user.application.service;
 
+import com.fifteen.eureka.user.application.dto.ApprovalRequestDto;
 import com.fifteen.eureka.user.application.dto.SignupRequestDto;
 
 public interface UserService {
 
   void registerUser(SignupRequestDto signupRequestDto);
+
+  void updateApprovalStatus(Long userId, ApprovalRequestDto requestDto);
+
 }
