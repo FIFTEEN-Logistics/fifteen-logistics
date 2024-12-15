ed76f4bcdf2c0461e8ce315e92a4d87e1662f198
hyeririjeon <jeonhyeri5555@gmail.com>
2024-12-15 21:03:06 +0900

    feat: order -> delivery 단일조회 추가(배달status 확인용) #3

--- vpo/src/main/java/com/fifteen/eureka/vpo/infrastructure/client/DeliveryClient.java
+++ vpo/src/main/java/com/fifteen/eureka/vpo/infrastructure/client/DeliveryClient.java
@@ -3,12 +3,19 @@ package com.fifteen.eureka.vpo.infrastructure.client;
 import com.fifteen.eureka.common.response.ApiResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.UUID;
 
 @FeignClient(name = "delivery-service")
 public interface DeliveryClient {
 
     @PostMapping("/api/deliveries")
     public ResponseEntity<ApiResponse<?>> createDelivery(@RequestBody CreateDeliveryDto createDeliveryDto);
+
+    @GetMapping("/api/deliveries/{deliveryId}")
+    public ApiResponse<?> getDelivery(@PathVariable UUID deliveryId);
 }
