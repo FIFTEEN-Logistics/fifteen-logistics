44b8f0e363bbc64578876a687897e87f89f11d91
sojinchoi <dvega.lyla@gmail.com>
2024-12-17 07:05:56 +0900

    feat: 배송 관리자 수정 삭제 기능 추가 #10

--- user/src/main/java/com/fifteen/eureka/user/application/dto/user/UserUpdateRequestDto.java
+++ user/src/main/java/com/fifteen/eureka/user/application/dto/user/UserUpdateRequestDto.java
@@ -4,6 +4,7 @@ import com.fifteen.eureka.common.role.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,5 +26,7 @@ public class UserUpdateRequestDto {
           message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character."
   )
   private String password;
+
+  private UUID hubId;
 }
 
