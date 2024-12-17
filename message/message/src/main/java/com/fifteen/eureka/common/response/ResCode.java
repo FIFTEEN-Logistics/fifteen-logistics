b0dae85d36bc46fda3f62bf403677c0989052cc1
hyeririjeon <jeonhyeri5555@gmail.com>
2024-12-17 20:35:46 +0900

    fix: recover message project

--- message/message/src/main/java/com/fifteen/eureka/common/response/ResCode.java
+++
@@ -1,8 +0,0 @@
-package com.fifteen.eureka.common.response;
-
-public interface ResCode {
-
-  Integer getHttpStatusCode();
-  Integer getCode();
-  String getMessage();
-}
--- message/message/src/main/java/com/fifteen/eureka/common/role/Role.java
+++
@@ -1,9 +0,0 @@
-package com.fifteen.eureka.common.role;
-
-public enum Role {
-  ROLE_ADMIN_MASTER,
-  ROLE_ADMIN_VENDOR,
-  ROLE_ADMIN_HUB,
-  ROLE_DELIVERY_HUB,
-  ROLE_DELIVERY_VENDOR;
-}
